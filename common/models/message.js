a9e78ca87d99d6be35758ea9e00d434520d49a80
Louis Moreau <louis.moreau@sigfox.com>
2017-03-13 11:32:26 +0100

    updated bower.json

--- common/models/message.js
+++ common/models/message.js
@@ -1,6 +1,7 @@
 'use strict';
 var request = require('request');
 var app = require('../../server/server.js');
+var Twitter = require('twitter');
 
 module.exports = function (Message) {
 
@@ -38,6 +39,19 @@ module.exports = function (Message) {
 
     next();
   });
+
+  Message.remoteMethod(
+    'twitterbird',
+    {
+      accepts: { arg: 'data', type: 'object', http: { source: 'body' }},
+      http: {path: '/twitterbird', verb: 'post'},
+      returns: {arg: 'deviceId', type: 'Object', root: true}
+    }
+  );
+
+  Message.twitterbird = function (data, cb) {
+      twit(data.message);
+  }
 };
 
 
@@ -167,6 +181,21 @@ function getDecimalCoord(sigfoxFrame) {
 
 }
 
-function backendRequest(url) {
+function twit(message){
+  //Use the environment variables in production
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  });
+
+  var status = "Hello World";
+
+  client.post('statuses/update', {status: status},  function(error, tweet, response){
+    if(error) console.log(error);
+    console.log(tweet);  // Tweet body.
+    console.log(response);  // Raw response object.
+  });
 
 }
