ccac0f32ce023d0f87558d75017a7d66449e7484
Louis Moreau <louis.moreau@sigfox.com>
2017-03-17 15:46:16 +0100

    added load parser bootscript

---
+++ server/boot/03-load-parsers.js
@@ -0,0 +1,58 @@
+'use strict';
+
+// to enable these logs set `DEBUG=boot:01-load-settings` or `DEBUG=boot:*`
+var log = require('debug')('boot:01-load-parsers');
+
+module.exports = function (app) {
+
+  var Parser = app.models.Parser;
+
+  function loadDefault() {
+    console.error('Creating default parsers');
+
+    var parsers = [
+      {
+        "name": "Geoloc Wifi",
+        "description": "Using the 2 strongest wifi MAC addresses near the device to call Google API to get geolocalisation position"
+      },
+      {
+        "name": "Sensit",
+        "description": "Decode the sensit sent data"
+      },
+      {
+        "name": "Tuto GPS",
+        "description": "Decode the GPS data from the tutorial: https://www.instructables.com/id/Sigfox-GPS-Tracker/"
+      }
+    ];
+
+    parsers.forEach(function (parser) {
+      Parser.create(parser, function (err) {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
+  }
+
+  function loadExisting() {
+    console.error('Loading existing parsers');
+
+    Parser.find(function (data) {
+      log(data);
+    });
+  }
+
+
+  Parser.count(function (err, result) {
+    if (err) {
+      console.error(err);
+    }
+    if (result < 1) {
+      loadDefault();
+    } else {
+      loadExisting();
+    }
+  });
+
+
+};
